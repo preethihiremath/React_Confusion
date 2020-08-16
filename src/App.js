e172358a5d967c8eccef22e1013535f1f8c9aba1
preethi_hiremath <preethivhiremath.vh@gmail.com>
2020-08-16 18:24:14 +0530

    component 2

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import logo from './logo.svg';
 import { Navbar ,NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponents';
+import { DISHES } from './shared/dishes';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
       <div >
@@ -15,7 +22,7 @@ class App extends Component {
           </div>
         </Navbar>
 
-    <Menu />
+    <Menu  dishes={this.state.dishes} />
    
       </div>
     );

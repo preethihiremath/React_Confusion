1ea09ca9934138f82dce688a601e3a7efbb5092e
preethi_hiremath <preethivhiremath.vh@gmail.com>
2020-08-20 13:14:32 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Navbar ,NavbarBrand } from 'reactstrap';
 import './App.css';
-import Menu from './components/MenuComponents';
-import { DISHES } from './shared/dishes';
-
+import { BrowserRouter } from 'react-router-dom';
+import Main from './components/MainComponent';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
-  render() {
-    return (
-      <div >
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
 
-    <Menu  dishes={this.state.dishes} />
-   
-      </div>
-    );
-  }
-}
+        render() {
+          return (
+            <BrowserRouter>
+            <div className="App">
+              <Main/>
+            </div>
+            </BrowserRouter>
+          );
+        }
+      }
+
 
 export default App;
